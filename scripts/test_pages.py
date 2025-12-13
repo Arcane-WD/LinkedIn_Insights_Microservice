af90eff240ee12ec0f05fcea66735caba980be31
ArcaneWD <harsha2004alpur@gmail.com>
2025-12-13 15:26:14 +0530

    using ghb dsktp

--- scripts/test_pages.py
+++ scripts/test_pages.py
@@ -1,53 +1,53 @@
-from linkedin_scraper import Company, actions
-from selenium import webdriver
-driver = webdriver.Chrome()
-import time
-import json
+# from linkedin_scraper import Company, actions
+# from selenium import webdriver
+# driver = webdriver.Chrome()
+# import time
+# import json
 
-email = "vrhr.olsi@gmail.com"
-password = "Harrsha_J@1911"
-actions.login(driver, email, password) # if email and password isnt given, it'll prompt in terminal
+# email = "vrhr.olsi@gmail.com"
+# password = "Harrsha_J@1911"
+# actions.login(driver, email, password) # if email and password isnt given, it'll prompt in terminal
 
-company_url_header = "https://www.linkedin.com/company/"
+# company_url_header = "https://www.linkedin.com/company/"
 
-companies = ["google", "microsoft", "deepsolv", "oracle", "meta", "electronic-arts"]
+# companies = ["google", "microsoft", "deepsolv", "oracle", "meta", "electronic-arts"]
 
-results = []
+# results = []
 
-for comp in companies:
-    url = company_url_header+comp
-    print(f"Scraping: {url}")
+# for comp in companies:
+#     url = company_url_header+comp
+#     print(f"Scraping: {url}")
 
-    company = Company(
-        linkedin_url=url,
-        driver=driver,
-        scrape=True,
-        get_employees=False,
-        close_on_complete=False
-    )
+#     company = Company(
+#         linkedin_url=url,
+#         driver=driver,
+#         scrape=True,
+#         get_employees=False,
+#         close_on_complete=False
+#     )
 
-    time.sleep(3)
+#     time.sleep(3)
 
-    company_data = {
-        "page_id": comp,
-        "name": company.name,
-        "linkedin_url": url,
-        "about_us": company.about_us,
-        "industry": company.industry,
-        "company_type": company.company_type,
-        "company_size": company.company_size,
-        "headcount": company.headcount,
-        "website": company.website,
-        "phone": company.phone,
-        "headquarters": company.headquarters,
-        "founded": company.founded,
-        "specialties": company.specialties,
-    }
+#     company_data = {
+#         "page_id": comp,
+#         "name": company.name,
+#         "linkedin_url": url,
+#         "about_us": company.about_us,
+#         "industry": company.industry,
+#         "company_type": company.company_type,
+#         "company_size": company.company_size,
+#         "headcount": company.headcount,
+#         "website": company.website,
+#         "phone": company.phone,
+#         "headquarters": company.headquarters,
+#         "founded": company.founded,
+#         "specialties": company.specialties,
+#     }
 
-    results.append(company_data)
+#     results.append(company_data)
 
-driver.quit()
+# driver.quit()
 
 
-with open("companies.json", "w", encoding="utf-8") as f:
-    json.dump(results, f, indent=2, ensure_ascii=False)
+# with open("companies.json", "w", encoding="utf-8") as f:
+#     json.dump(results, f, indent=2, ensure_ascii=False)
