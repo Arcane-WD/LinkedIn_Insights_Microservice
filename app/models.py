32be87efce3e7d81f3e72724e557bd6fe5b7eb65
ArcaneWD <harsha2004alpur@gmail.com>
2025-12-13 16:23:45 +0530

    json to sqlite
    learn and shifted json files to sqlite database for further FastAPI testing.

---
+++ app/models.py
@@ -0,0 +1,24 @@
+from database import Base
+from sqlalchemy import Column, Integer, String, Text
+
+class CompModel(Base):
+    __tablename__ = "companies"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    page_id = Column(String, unique=True, index=True)
+    name = Column(String, index=True)
+    linkedin_url = Column(String)
+
+    about_us = Column(Text)
+    industry = Column(String)
+    company_type = Column(String)
+    company_size = Column(String)
+    headcount = Column(Integer)
+
+    website = Column(String)
+    phone = Column(String)
+    headquarters = Column(String)
+    founded = Column(String)
+
+    specialties = Column(Text) 
